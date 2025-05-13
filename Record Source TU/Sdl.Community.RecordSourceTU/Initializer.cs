97d31a2c17643761dc68899ec7184e187c9b2e91
multifarious <6784675+paulfilkin@users.noreply.github.com>
2025-05-13 19:04:26 +0200

    Spring Cleaned Version

---
+++ Record Source TU/Sdl.Community.RecordSourceTU/Initializer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.Extensions;
+
+namespace Sdl.Community.RecordSourceTU
+{
+    [ApplicationInitializer]
+    public class Initializer : IApplicationInitializer
+    {
+        public void Execute()
+        {
+            var persistance = new Persistance();
+
+            var addSourceTmConfiguration = persistance.Load();
+            if (addSourceTmConfiguration != null) return;
+
+            persistance.Save(new AddSourceTmConfigurations() {Configurations = new List<AddSourceTmConfiguration>()});
+        }
+    }
+}
